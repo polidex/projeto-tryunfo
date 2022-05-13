0e294bbfd2ea6de5bb927584db20241fd6f80b11
polidex <lp.igor@gmail.com>
2022-05-13 10:06:03 -0300

    1 done

---
+++ src/components/Form.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" data-testid="name-input" />
+        <textarea data-testid="description-input" />
+        <text type="number" data-testid="attr1-input" />
+        <text type="number" data-testid="attr2-input" />
+        <text type="number" data-testid="attr3-input" />
+        <text data-testid="image-input" />
+        <label htmlFor="raridade">
+          Escolha a raridade:
+          <select name="raridade" data-testid="rare-input">
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito raro</option>
+          </select>
+        </label>
+        <checkbox data-testid="trunfo-input" />
+        <button type="button" data-testid="save-button">Salvar</button>
+      </div>
+    );
+  }
+}
+
+export default Form;
